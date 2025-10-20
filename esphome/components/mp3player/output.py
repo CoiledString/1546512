a815285c7cc55e418ff718feed6d55012a52bffd
CoiledString <68668823+CoiledString@users.noreply.github.com>
2025-10-20 01:54:01 +0100

    Update output.py

--- esphome/components/mp3player/output.py
+++ esphome/components/mp3player/output.py
@@ -1,12 +1,8 @@
 from esphome.components import output
 import esphome.codegen as cg
+import esphome.config_validation as cv  # ✅ This is the missing import
 
 mp3player_ns = cg.esphome_ns.namespace("mp3player")
 MP3PlayerOutput = mp3player_ns.class_("MP3PlayerOutput", cg.Component, output.BinaryOutput)
 
-CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend({}).extend(cg.COMPONENT_SCHEMA)
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[output.CONF_ID])
-    await output.register_output(var, config)
-    await cg.register_component(var, config)
+CONFIG_SCHEMA = output.BINARY_OUTPUT_SCHEMA.extend({}).extend(cv.COMPONENT_SCHEMA)  # ✅ Use cv here
